8a2381c97561bf0ab539f849822cfb298d18735c
Konstantin @ks <ks@divine.band>
2025-03-24 16:19:37 +0300

    Specify liquidity recipient on failure

--- wrappers/VaultNative.ts
+++ wrappers/VaultNative.ts
@@ -48,7 +48,7 @@ export class VaultNative implements Contract {
             amount,
             params,
             new PoolCreationParams(
-                new PublicPoolCreationParams(recipient, notification_data),
+                new PublicPoolCreationParams(recipient, false, notification_data),
                 new PrivatePoolCreationParams(true, null)
             )
         )
--- wrappers/types.ts
+++ wrappers/types.ts
@@ -138,6 +138,7 @@ export class PoolParams extends CellSerializable {
 export class PublicPoolCreationParams extends CellSerializable {
     constructor(
         public recipient: Address,
+        public use_recipient_on_failure: boolean = false,
         public notification_data: NotificationData | null = null
     ) {
         super()
@@ -145,6 +146,7 @@ export class PublicPoolCreationParams extends CellSerializable {
 
     public write(b: Builder): void {
         b.storeAddress(this.recipient)
+        b.storeBit(this.use_recipient_on_failure);
         b.storeMaybeRef(this.notification_data?.toCell())
     }
 }
@@ -418,25 +420,21 @@ export class DepositLiquidityParamsTrimmed extends CellSerializable {
         public min_lp_amount: bigint,
         public recipient: Address | null,
         public referral: Address | null,
-        public notification_data: NotificationData | null
+        public notification_data: NotificationData | null,
+        public use_recipient_on_failure: boolean = false
     ) {
         super()
     }
 
     public write(b: Builder): void {
-        b
-            .storeAddress(this.recipient)
+        b.storeAddress(this.recipient)
+            .storeBit(this.use_recipient_on_failure)
             .storeAddress(this.referral)
             .storeUint(this.deadline, 32)
             .storeUint(1, 2)
             .storeCoins(this.min_lp_amount)
             .storeMaybeRef(null)
             .storeMaybeRef(this.notification_data?.toCell());
-        // b.storeUint(this.deadline, 32)
-        //     .storeCoins(this.min_lp_amount)
-        //     .storeAddress(this.recipient)
-        //     .storeAddress(this.referral)
-        //     .storeMaybeRef(this.notification_data?.toCell());
     }
 }
 
